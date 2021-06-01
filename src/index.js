18b1cdd30a017fca1f652f659d25514a0d56e664
KIYO1234 <getstarted3601@gmail.com>
2021-06-01 17:55:42 +0900

    再修正

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from '../src/components/App';
+import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 import {Provider} from 'react-redux'
 // 元々はstore/indexを作ってた
